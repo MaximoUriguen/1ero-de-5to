01b326fb4f66e0b0beb0a2f19c40165ceee46e13
alumno <alumno@Instituto.local>
2025-03-18 09:51:57 -0300

    variable condicional
    dawdad

--- java.js
+++ java.js
@@ -2,5 +2,12 @@ let mensajealerta = "Hola bienvenido"
 alert(mensajealerta)
 let añoNacimiento = 2009
 let añoActual = 2025
-let edad = añoActual-añoNacimiento
+let edad = añoActual - añoNacimiento
 alert("mi edad es " + edad)
+alert("tenes que pasar al boliche, decime tu nobre  para ver si estas en lista y tu edad")
+let nombreAlumno = prompt ("ingresa tu nombre")
+let edadd = prompt("ingresa tu edad solo numeros")
+if (edadd >= 18) {
+    alert("podes pasar")
+
+}
